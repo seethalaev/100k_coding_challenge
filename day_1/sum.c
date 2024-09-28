227871f5e390b62163f63afdf772c5827b1b9bfc
seethala e v <sheethalev@gmail.com>
2024-09-28 22:58:17 +0530

    sum programme created

---
+++ day_1/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;float sum;float f;
+    printf("Enter two numbers");
+    scanf("%d%f%f",&a,&f,&sum);
+    sum=a+f;
+    printf("sum is: %f",sum);
+    return 0;
+}
