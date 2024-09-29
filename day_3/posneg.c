f5c7509c2c224c4f89529276827632d637ff4265
seethala e v <sheethalev@gmail.com>
2024-09-29 17:25:57 +0530

    posistitve negative pgm added in day_3

---
+++ day_3/posneg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int num;
+printf("enter a number");
+scanf("%d",&num);
+if(num<0)
+    {
+    printf("number is negative");
+    }
+else
+    {
+    printf("number is positive");
+    }
+}
