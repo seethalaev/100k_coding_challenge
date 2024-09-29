dfc7887f70209cee23e8b14a7fe569bf3296f7d2
seethala e v <sheethalev@gmail.com>
2024-09-29 17:45:33 +0530

    gratest number when from two numbers done

---
+++ day_3/greater.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int num1,num2;
+printf("enter numbers");
+scanf("%d%d",&num1,&num2);
+    if(num1>num2)
+    {
+        printf("num1 is greater");
+    }
+    else
+    {
+        printf("num2 is greater");
+    }
+}
