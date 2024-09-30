8b70416cac414404eadfcc1295fd44e8a62be009
seethala e v <sheethalev@gmail.com>
2024-09-30 22:38:40 +0530

    sum of natural numbers heding modified

--- day_4/sumofnaturalnbrs.c
+++ day_4/sumofnaturalnbrs.c
@@ -2,14 +2,11 @@
 #include<stdlib.h>
 int main()
 {
-    int i,sum=0,n;
+    int a;
     {
-        printf("enter a limit\n");
-        scanf("%d",&n);
-        for(i=0;i<=n;i++)
-        {
-        sum=sum+i;
-        }
-        printf("sum=%d",sum);
-   }
+        printf("enter a number\n");
+        scanf("%d",&a);
+       {
+        
+       }
 }
