3eac77e5a4356a655ec755303755645e58bc5bd5
seethala e v <sheethalev@gmail.com>
2024-10-01 13:04:12 +0530

    star  printed simply

---
+++ day_4/simplestar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,limit=4,j;
+    {
+        for(i=0;i<limit;i++)
+        {
+            for(j=0;j<i;j++)
+        
+            printf("*");
+        {  
+             printf("\n");
+        }
+        }
+        
+    }    
+}
