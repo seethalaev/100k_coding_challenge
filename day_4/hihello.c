421004479bf71204ee96790eaae33468e2cf3445
seethala e v <sheethalev@gmail.com>
2024-10-01 13:27:09 +0530

    hihello  printed simply

---
+++ day_4/hihello.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int i,j;
+    {
+        for(i=0;i<10;i++)
+        {
+           printf("hi");
+           if(i==5)
+           {
+               break;
+           }
+        printf("hello \n");
+        }
+        printf("finished");
+    }
+}
