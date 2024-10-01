4d4dd77838dbf575291b3b8eb52f2da2424f332e
seethala e v <sheethalev@gmail.com>
2024-10-01 17:19:50 +0530

    star reverse  printed simply

---
+++ day_4/starrev.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int i,j,limit=5;
+    {
+        for(i=limit;i>0;i--)
+            {
+                for(j=0;j<i;j++)
+                {
+                    printf("*");
+                }
+            
+             printf("\n");
+        }}
+    }
