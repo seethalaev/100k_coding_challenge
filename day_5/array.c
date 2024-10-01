986272b848bd6667c0f852d1ee5bad49b58ead12
seethala e v <sheethalev@gmail.com>
2024-10-02 00:06:57 +0530

    simple array  printed

---
+++ day_5/array.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int a[5];
+    int i;
+    {
+        printf("enter values \n");
+       { 
+            for(i=0;i<5;i++)
+         scanf("%d",&a[i]);
+        }
+        
+        printf("entered values are \n");
+        
+            for(i=0;i<5;i++)
+            {
+            printf("%d\t",a[i]);
+    }       }
+}
