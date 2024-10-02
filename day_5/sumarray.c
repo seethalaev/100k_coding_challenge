0eec626343d4a16d524cb1741f99577475523f3d
seethala e v <sheethalev@gmail.com>
2024-10-02 13:05:29 +0530

    sumarray  printed

---
+++ day_5/sumarray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int limit,i,values[100],sum=0;
+    printf("enter limit");
+    scanf("%d",&limit);
+    printf("enter values");
+    {
+        for(i=0;i<limit;i++)
+        scanf("%d",&values[i]);
+    }
+    for(i=0;i<limit;i++)
+    {
+    sum=sum+values[i];
+
+    }
+    
+    printf("%d",sum);
+  
+}
