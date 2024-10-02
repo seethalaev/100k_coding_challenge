03d00c1e47fa800d8fe0db4db2a7b5b4f4b126ce
seethala e v <sheethalev@gmail.com>
2024-10-02 10:13:01 +0530

    array  printed secondtime

---
+++ day_5/array2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+  int a[5];
+  int i,limit;
+  printf("enter limits of array");
+  scanf("%d",&limit);
+  {
+      
+  
+  printf("enter values\n");
+  {
+      for(i=0;i<limit;i++)
+      scanf("%d",&a[i]);
+      
+  }
+  printf("enterd values are: \n");
+  {
+      for(i=0;i<limit;i++)
+    printf("%d\t",a[i]);
+
+}}}
+  
