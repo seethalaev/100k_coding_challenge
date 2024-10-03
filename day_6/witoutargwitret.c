cc5ee97a9fd709171f707d978c3c211f8031125f
seethala e v <sheethalev@gmail.com>
2024-10-03 21:08:54 +0530

    without argument with return

---
+++ day_6/witoutargwitret.c
@@ -0,0 +1,18 @@
+*******************************************************************************/
+#include<stdio.h>
+int sum();
+int main()
+{
+    int k;
+    k=sum();
+    printf("sum is %d",k);
+    return 0;
+}
+int sum()
+{
+    int a,b,sum;
+    printf("enter 2 numbers\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    return sum;
+}
