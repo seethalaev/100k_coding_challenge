3dfb8b8b9cbe615e6143d18a21665ab346238fb2
seethala e v <sheethalev@gmail.com>
2024-10-03 10:54:02 +0530

    multidimaensional array printed

---
+++ day_6/multidimarray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,values[3][3];
+    printf("enter values");
+     for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+            scanf("%d",&values[i][j]);
+            }
+        }
+    
+    printf("entered array is");
+    
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+        {
+        printf("%d\t",values[i][j]);
+        }
+            printf("\n");
+        }
+}
