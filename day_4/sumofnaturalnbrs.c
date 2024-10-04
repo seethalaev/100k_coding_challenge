137cf06ad09fc55f57d45a9bac9e06d67f73e68f
seethala e v <sheethalev@gmail.com>
2024-10-04 10:04:10 +0530

    sum of natural numbers

--- day_4/sumofnaturalnbrs.c
+++ day_4/sumofnaturalnbrs.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-#include<stdlib.h>
 int main()
+#include<stdio.h>
+#include<stdlib.h>
 {
+    int i,sum=0,n;
     int a;
     {
-        printf("enter a number\n");
-        scanf("%d",&a);
-       {
-        
-       }
+        printf("enter a limit\n");
+        scanf("%d",&n);
+        for(i=0;i<=n;i++)
+        {
+        sum=sum+i;
+        }
+        printf("sum=%d",sum);
+   }
 }
