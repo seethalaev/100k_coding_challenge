66d6c66e73f21d36ca6a2da7fa07ab08a79f275a
seethala e v <sheethalev@gmail.com>
2024-10-13 00:18:42 +0530

    array  pgm edited

--- day_5/array.c
+++ day_5/array.c
@@ -3,16 +3,16 @@ int main()
     int a[5];
     int i;
     {
-        printf("enter values \n");
-       { 
-            for(i=0;i<5;i++)
-         scanf("%d",&a[i]);
+    printf("enter values \n");
+    for(i=0;i<5;i++)
+        {
+            scanf("%d",&a[i]);
         }
-        
-        printf("entered values are \n");
-        
-            for(i=0;i<5;i++)
-            {
-            printf("%d\t",a[i]);
-    }       }
+        printf("i");
+    printf("entered values are \n");
+    for(i=0;i<5;i++)
+        {
+        printf("%d\t",a[i]);
+        }       
+    }
 }
