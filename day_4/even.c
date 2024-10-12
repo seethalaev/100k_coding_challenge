3fc0f8cddf216ca14523f774607710d1a906bda2
seethala e v <sheethalev@gmail.com>
2024-10-13 00:17:12 +0530

    even pgm edited

--- day_4/even.c
+++ day_4/even.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include<stdlib.h>
+
 int main()
 {
     int limit=10,i;
    
-        for(int i=2;i<=limit;i++)
+        for(int i=2;i<=limit;i+=2)
         {
            printf("%d \n",i); 
         }
