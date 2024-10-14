c41c3372874c539208185a44e704a6bb469deddb
seethala e v <sheethalev@gmail.com>
2024-10-14 21:31:11 +0530

    simple star

--- day_4/simplestar.c
+++ day_4/simplestar.c
@@ -9,7 +9,7 @@ int main()
         for(j=0;j<i;j++)
                 {
                     printf("inside second loop");
-                      printf("*"); 
+                     printf("*"); 
                 }
              printf("\n");
         }      
