75d2c5a5fdceb5ebf7e0d772728447d392ed8e43
seethala e v <sheethalev@gmail.com>
2024-10-14 21:34:11 +0530

    loop edited lines

--- day_6/loop.c
+++ day_6/loop.c
@@ -4,10 +4,10 @@ int main()
 {
     for(int i=1;i<5;i++)
     {
-        printf("inside first loop ");
+        printf("seethu");
         for(int j=1;j<i;j++)
             {
-            printf("inside second loop ");
+            printf("ilpa ");
             printf("*");
             }
         printf("\n");
