49fc464909041c2e4a25749f448490a41a38d45c
seethala e v <sheethalev@gmail.com>
2024-10-14 21:31:55 +0530

    star reverse

--- day_4/starrev.c
+++ day_4/starrev.c
@@ -4,11 +4,14 @@ int main()
     {
         for(i=limit;i>0;i--)
             {
+                 //printf("seethu");
                 for(j=0;j<i;j++)
                 {
+                  //   printf("muthu");
                     printf("*");
                 }
             
              printf("\n");
-        }}
+        }
     }
+}
