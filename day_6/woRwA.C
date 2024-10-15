97f8bbe2f67940c006107913b09ca857cb02cde9
seethala e v <sheethalev@gmail.com>
2024-10-15 23:41:27 +0530

    without ret with argument second pgm

---
+++ day_6/woRwA.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void sum(int a,int b,int c); //definition
+int main()//main function creation
+{
+    int mark1,mark2,mark3;
+    printf("enter mark1: ");
+    scanf("%d",&mark1);
+     printf("enter mark2: ");
+    scanf("%d",&mark2);
+     printf("enter mark3: ");
+    scanf("%d",&mark3);
+    sum(mark1,mark2,mark3);//function call
+    //printf("entered marks %d%d%d",mark1,mark2,mark3);
+}
+void sum(int a,int b,int c)//sum function creation
+{
+    
+    int total=a+b+c;
+    printf("mark total %d",total);
+}
