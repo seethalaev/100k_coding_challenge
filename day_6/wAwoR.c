46fb26a2a670591bdceb69c25c0c79f0a898108c
seethala e v <sheethalev@gmail.com>
2024-10-16 16:36:39 +0530

    with argument  without argument pgm

---
+++ day_6/wAwoR.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void mark(int mark1,int mark2);
+int main()
+{
+  int rollno,a,b;
+  printf("enter roll\n");
+  scanf("%d",&rollno);
+  mark(a,b);
+}
+void mark(int mark1,int mark2)
+{
+    int m1,m2;
+    printf("enter marks\n");
+    scanf("%d%d",&m1,&m2);
+    
+}
