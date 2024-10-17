dade389e9eb66bfe2b4908187ed5fa61e9f247ba
seethala e v <sheethalev@gmail.com>
2024-10-17 23:26:06 +0530

    bank pgm in self learning deleted lines

--- self_7/bank.c
+++ self_7/bank.c
@@ -15,24 +15,3 @@ void get_account_balance()
 }
 
 
-#include <stdio.h>
-void get_account_balance();
-void withdraw_money(int money);
-
-char name[50]="shilpa";
-int acountblnce=12000;
-
-int main()
-{
-  get_account_balance();
-  withdraw_money(500);
-}
-
-void get_account_balance()
-{
-    printf("your account balance is %d\n",acountblnce);
-}
-void withdraw_money(int money)
-{
-    money=acountblnce-500;
-}
