435bcb0d67a1573ccb1191bdde10d9f49d722172
seethala e v <sheethalev@gmail.com>
2024-10-17 23:26:58 +0530

    bank pgm in self learng updated pgm

--- self_7/bank.c
+++ self_7/bank.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 void get_account_balance();
+void withdraw(int a);
 
 char name[50]="shilpa";
 int acountblnce=12000;
 
+
 int main()
 {
   get_account_balance();
+  withdraw(500);
 }
+
 
 void get_account_balance()
 {
-    printf("your account balance is %d",acountblnce);
+    printf("your account balance is %d\n",acountblnce);
 }
-
-
+void withdraw(int a)
+{
+    acountblnce=acountblnce-a;
+    
+}
