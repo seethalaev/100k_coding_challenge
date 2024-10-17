3bf9a66e8d2e65da20f18b6feb739e6020a0f887
seethala e v <sheethalev@gmail.com>
2024-10-17 23:25:05 +0530

    bank pgm in self learning

--- self_7/bank.c
+++ self_7/bank.c
@@ -13,3 +13,26 @@ void get_account_balance()
 {
     printf("your account balance is %d",acountblnce);
 }
+
+
+#include <stdio.h>
+void get_account_balance();
+void withdraw_money(int money);
+
+char name[50]="shilpa";
+int acountblnce=12000;
+
+int main()
+{
+  get_account_balance();
+  withdraw_money(500);
+}
+
+void get_account_balance()
+{
+    printf("your account balance is %d\n",acountblnce);
+}
+void withdraw_money(int money)
+{
+    money=acountblnce-500;
+}
