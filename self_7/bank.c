7e077384c4eb7388fde29c25d4af23030db78d3e
seethala e v <sheethalev@gmail.com>
2024-10-17 23:47:38 +0530

    bank pgm in self learng 3 nd funcion added

--- self_7/bank.c
+++ self_7/bank.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 void get_account_balance();
 void withdraw(int a);
+int deposit_money(int b);
 
 char name[50]="shilpa";
 int acountblnce=12000;
@@ -10,9 +11,8 @@ int main()
 {
   get_account_balance();
   withdraw(500);
+  deposit_money(10);
 }
-
-
 void get_account_balance()
 {
     printf("your account balance is %d\n",acountblnce);
@@ -22,4 +22,12 @@ void withdraw(int a)
     printf("Money withdrawn %d\n",a);
     acountblnce=acountblnce-a;
     printf("Account balance after money withdrwal %d\n",acountblnce);
+}
+ int deposit_money(int b)
+{
+   acountblnce=acountblnce+b;
+   printf("Deposit money you want to \n");
+   scanf("%d",&b);
+   return acountblnce;
+  
 }
