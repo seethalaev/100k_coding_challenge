a3c8df8b492664fa9dce0c8a94d0771f5c048855
seethala e v <sheethalev@gmail.com>
2024-10-18 21:53:19 +0530

    withdarw , add ,balance function added

--- self_7/bank.c
+++ self_7/bank.c
@@ -1,7 +1,15 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 void get_account_balance();
 void withdraw(int a);
-int deposit_money(int b);
+void deposit_money(int b);
 
 char name[50]="shilpa";
 int acountblnce=12000;
@@ -11,8 +19,8 @@ int main()
 {
   get_account_balance();
   withdraw(500);
-  deposit_money(10);
-   get_account_balance();
+  deposit_money(20);
+  get_account_balance();
 }
 void get_account_balance()
 {
@@ -22,13 +30,8 @@ void withdraw(int a)
 {
     printf("Money withdrawn %d\n",a);
     acountblnce=acountblnce-a;
-    printf("Account balance after money withdrwal %d\n",acountblnce);
 }
- int deposit_money(int b)
+ void deposit_money(int b)
 {
-  
-   printf("Deposit money want to \n");
-   scanf("%d",&b);
-   acountblnce=acountblnce+b;
-   return acountblnce;
+ acountblnce=acountblnce+b;
 }
