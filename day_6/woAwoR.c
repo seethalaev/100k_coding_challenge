a21c6e2b0010d6749a1e6ff146064aaa2149f278
seethala e v <sheethalev@gmail.com>
2024-10-21 22:10:47 +0530

    without argument without  return  pgm

---
+++ day_6/woAwoR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void greeting(); // definition
+int main() // main function creation
+{
+  printf("this is main function\n");
+  greeting();
+}
+
+void greeting() // greeting function creation
+{
+    printf("good morning\n");
+}
