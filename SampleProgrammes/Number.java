e47cfb9031b670e02f52d2d3ac0b1c499bed1b71
seethala e v <sheethalev@gmail.com>
2024-10-22 11:04:59 +0530

    Number printing

---
+++ SampleProgrammes/Number.java
@@ -0,0 +1,15 @@
+package SampleProgrammes;
+
+import java.util.Scanner;
+
+public class Number 
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);//ctrl+space
+        System.out.println("enter two number");
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println(""+a+" "+b);
+    }
+}
