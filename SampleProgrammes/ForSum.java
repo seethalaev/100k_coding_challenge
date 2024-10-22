35b3e0659176d39619dc6433792d5010f57de49d
seethala e v <sheethalev@gmail.com>
2024-10-22 11:00:37 +0530

    sum with for loop

---
+++ SampleProgrammes/ForSum.java
@@ -0,0 +1,19 @@
+package SampleProgrammes;
+
+import java.util.Scanner;
+
+public class ForSum {
+    public static void main(String[] args)
+    {
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter limit");
+    int limit=s.nextInt();
+    int sum=0;
+
+    for(int i=0;i<limit;i++)
+    {
+        sum=sum+i;
+    }
+    System.out.println("Result is :"+sum);
+    }  
+}
