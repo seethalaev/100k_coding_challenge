2878e8ac8d7827b1f2d5da2bdfc7356f3932cc78
seethala e v <sheethalev@gmail.com>
2024-10-22 11:04:00 +0530

    integer printing prgram

---
+++ SampleProgrammes/Int.java
@@ -0,0 +1,10 @@
+package SampleProgrammes;
+
+public class Int 
+{
+    public static void main (String[] ar)
+    {
+        int a=19;
+        System.out.println("Interger values is :"+a);
+    }
+}
