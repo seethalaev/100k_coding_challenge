2702674a481796ac51d1551b2cc9685c422a1bdd
seethala e v <sheethalev@gmail.com>
2024-10-22 11:06:06 +0530

    sum printing

---
+++ SampleProgrammes/Sum.java
@@ -0,0 +1,18 @@
+package SampleProgrammes;
+
+import java.util.Scanner;
+
+public class Sum
+ {
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter two number");
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int result=a+b;
+        System.out.println("Result is :"+result);
+
+
+    }        
+}
