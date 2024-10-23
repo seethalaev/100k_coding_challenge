6dd478f3c2de717a778257f0ac2c16f27f487026
seethala e v <sheethalev@gmail.com>
2024-10-23 21:31:05 +0530

    with argument and without argument method created in one class-Sum.java

---
+++ TwoClass3/Sum.java
@@ -0,0 +1,16 @@
+package TwoClass3;
+
+public class Sum 
+{
+   public static void main (String args[])
+   {
+void calculateSum(int k,int m)
+{
+    c=k+m;
+}
+void displaySum()
+{
+    System.out.println("c");
+}
+   }}
+
