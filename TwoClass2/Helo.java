0177da6fb45ca974e1e72554ac33eba840b6abd7
seethala e v <sheethalev@gmail.com>
2024-10-23 21:27:25 +0530

    object created,called in one class-Helo.java

---
+++ TwoClass2/Helo.java
@@ -0,0 +1,16 @@
+package TwoClass2;
+
+public class Helo
+{
+   public static void main(String[] ags) 
+   {
+    Samp s1=new Samp();
+    Samp s2=new Samp();
+    s1.a=10;
+    s1.b=50;
+    s2.a=50;
+    s2.b=60;
+    s1.display();
+    s2.display();
+}
+}
