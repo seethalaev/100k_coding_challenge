5f6577ce1843f475277f6b4911ab544d89cfc584
seethala e v <sheethalev@gmail.com>
2024-10-23 21:20:20 +0530

    Print integeer edited

---
+++ SampleProgrammes/PrintI.java
@@ -0,0 +1,10 @@
+package SampleProgrammes;
+
+public class PrintI
+{
+    public static void main (String[] arg)
+    {
+        int a=19;
+        System.out.println("Interger values is :"+a);
+    }
+}
