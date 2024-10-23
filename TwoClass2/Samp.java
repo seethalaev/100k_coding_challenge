fe8d6295867cf78a13623a0013cf76300dcbc312
seethala e v <sheethalev@gmail.com>
2024-10-23 21:26:13 +0530

    object,method created in one class-Samp.java

---
+++ TwoClass2/Samp.java
@@ -0,0 +1,14 @@
+package TwoClass2;
+
+public class Samp 
+{
+    public static void main(String[] args)
+    {
+        int a;
+        int b;
+        void display();
+        {
+        System.out.println("a");
+        }
+    }
+}
