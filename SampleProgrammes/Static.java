8d7b6956f1c461f2a8b3609a4e5781da79989e4b
seethala e v <sheethalev@gmail.com>
2024-10-27 22:56:26 +0530

    static pgm done

---
+++ SampleProgrammes/Static.java
@@ -0,0 +1,18 @@
+package SampleProgrammes;
+
+class Newi
+{
+static int a=10;
+}
+public class Static
+ {
+    public static void main (String  args[])
+    {
+        System.out.println(a);
+        hello();
+    }
+     static void hello()
+    {
+
+    }
+}
