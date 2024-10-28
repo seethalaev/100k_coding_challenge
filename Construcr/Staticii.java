a1804b2eb50c33f21b3a120fe1849becdfef5958
seethala e v <sheethalev@gmail.com>
2024-10-28 22:25:12 +0530

    constructor by ar

---
+++ Construcr/Staticii.java
@@ -0,0 +1,24 @@
+
+
+package Construcr;
+
+class Student {
+    static String name;  // Static variable shared by all Student objects
+    int mark;            // Instance variable unique to each object
+}
+
+public class Staticii {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "seethu";
+        s.mark = 50;
+
+        Student t = new Student();
+        t.name = "arun";  // Overwrites the static variable 'name' for all instances
+        t.mark = 100;
+
+        // Print values for both objects
+        System.out.println("Student s -> Name: " + s.name + ", Mark: " + s.mark);
+        System.out.println("Student t -> Name: " + t.name + ", Mark: " + t.mark);
+    }
+}
