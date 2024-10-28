003786b1d763700a603ca53ea56f7430a85cde03
seethala e v <sheethalev@gmail.com>
2024-10-28 22:29:06 +0530

    static prgm

---
+++ Day_8/Statici.java
@@ -0,0 +1,18 @@
+// package Day_8;
+
+
+// public class Statici
+//  {
+//     {
+//         static int a=10;
+//     }
+//     public static void main (String  args[])
+//     {
+//         System.out.println(a);  
+//         hello();
+//     }
+//      static void hello()
+//     {
+        
+//     }
+// }
