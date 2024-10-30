88a46f2d68511277e59f8382421d795e1a42e2d4
seethala e v <sheethalev@gmail.com>
2024-10-30 16:25:51 +0530

    hello is calling object

---
+++ Constrt/Hello.java
@@ -0,0 +1,10 @@
+package Constrt;
+
+public class Hello
+{
+   public static void main(String []args) //main -->ctrl+space
+   {
+     Sample s=new Sample();     //press ctrl key and click function
+//class name       //constructor name
+   }
+}
