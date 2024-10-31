f8a251afcf71e3d412df37ff60cb4603765ade3b
seethala e v <sheethalev@gmail.com>
2024-10-31 21:50:00 +0530

    Wi th constructor

---
+++ Superr/A.java
@@ -0,0 +1,16 @@
+package Superr;
+
+public class A 
+{
+    
+     int a;
+     
+     A()
+     {
+       System.out.println("A is a constructor");
+     } 
+     A(int a)
+     {
+      System.out.println("A is argument constructor");
+     }
+}
