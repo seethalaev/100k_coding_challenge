bbec2fab430be44456f42e160e9a10412b089014
seethala e v <sheethalev@gmail.com>
2024-10-31 21:48:01 +0530

    simple exxample for super

---
+++ Supr/A.java
@@ -0,0 +1,19 @@
+package Supr;
+
+public class A 
+{
+     int a;
+     void Display()
+     {
+        System.out.println("Wlcome to A");
+     }
+    //  A()
+    //  {
+    //    System.out.println("A is a constructor");
+    //  } 
+    //  A(int a)
+    //  {
+    //   System.out.println("A is argument constructor");
+    //  }
+}
+
