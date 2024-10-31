a377ed548090f670feded3ff439ee179cb1216d3
seethala e v <sheethalev@gmail.com>
2024-10-31 21:50:50 +0530

    with Dconstructor

---
+++ Superr/D.java
@@ -0,0 +1,20 @@
+package Superr;
+
+public class D extends A
+ {
+    
+    D()
+    {
+        System.out.println("D IS A CONSTRUCTOR");
+    }
+    D(int v)
+    {
+        //super(150);
+        System.out.println("Argument constructor of D");
+    }
+    
+public static void main(String[] args) 
+{
+    D d=new D(100);
+    
+}}
