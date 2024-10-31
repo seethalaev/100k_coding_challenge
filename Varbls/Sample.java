7376d78dc90a7ca07ee7dc36b2e9c5b7e924262e
seethala e v <sheethalev@gmail.com>
2024-10-31 10:01:41 +0530

    this keyword used

--- Varbls/Sample.java
+++ Varbls/Sample.java
@@ -2,10 +2,10 @@ package Varbls;
 
 public class Sample
 {
-int a=100,b=300;//global variable
-Sample(int a,int b)//local variable
+int a=100,b=300;//global variable can access to all fns
+Sample(int a,int b)//local variable in constructor
 {
-     a=this.a;
+     a=this.a;             //this represent glbal object for this cls(this.a=100,a=int a)
      this.b=b;
    System.out.println(this.a+" "+this.b);
 }}
