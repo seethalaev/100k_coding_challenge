35b2555e675c9dd2621ad8ba0ff295f3cc572d47
seethala e v <sheethalev@gmail.com>
2024-11-01 22:16:16 +0530

    lines added

--- Superr/D.java
+++ Superr/D.java
@@ -2,7 +2,6 @@ package Superr;
 
 public class D extends A
  {
-    
     D()
     {
         System.out.println("D IS A CONSTRUCTOR");
@@ -12,6 +11,7 @@ public class D extends A
         //super(150);
         System.out.println("Argument constructor of D");
     }
+   
     
 public static void main(String[] args) 
 {
