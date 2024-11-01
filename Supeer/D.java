a9e852dd47a5889224ffe32e2c85579b2e79fdba
seethala e v <sheethalev@gmail.com>
2024-11-01 22:13:53 +0530

    removed unwanted lines

--- Supeer/D.java
+++ Supeer/D.java
@@ -5,9 +5,7 @@ public class D extends A
     void Display()
     {        
        super.Display();     //super with method
-       super.a=10;            //super with var
-       int c=a+super.a;
-       System.out.println(c);
+       
     }
     public static void main(String[] args) 
     {
