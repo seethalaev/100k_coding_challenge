00da39cb211ba2068bda7b1a4931285b30095760
seethala e v <sheethalev@gmail.com>
2024-11-02 20:13:53 +0530

    removed

--- Superr/D.java
+++ Superr/D.java
@@ -6,11 +6,7 @@ public class D extends A
     {
         System.out.println("D IS A CONSTRUCTOR");
     }
-    D(int v)
-    {
-        //super(150);
-        System.out.println("Argument constructor of D");
-    }
+    
    
     
 public static void main(String[] args) 
