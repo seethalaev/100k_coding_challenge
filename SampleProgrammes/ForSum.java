fb1b92da004a65ad6ed3c914a42b0e37a9bdb959
seethala e v <sheethalev@gmail.com>
2024-11-04 22:16:51 +0530

    error

--- SampleProgrammes/ForSum.java
+++ SampleProgrammes/ForSum.java
@@ -6,12 +6,6 @@ public class ForSum {
     public static void main(String[] args)
     {
   
-    Scanner s=new Scanner(System.in);
-    System.out.println("Enter limit");
-    int limit=s.nextInt();
-    int sum=0;
-
-
     for(int i=0;i<limit;i++)
     {
         sum=sum+i;
