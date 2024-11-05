940e785ddaeca5946ef24867d295643abc726acf
seethala e v <sheethalev@gmail.com>
2024-11-05 21:54:33 +0530

    global variables pgm done

--- Varbls/Sample.java
+++ Varbls/Sample.java
@@ -5,5 +5,7 @@ public class Sample
 int a=100,b=300;//global variable can access to all fns
 Sample(int a,int b)//local variable in constructor
 {
-   
+     a=this.a;             //this represent glbal object for this cls(this.a=100,a=int a)
+     this.b=b;
+   System.out.println(this.a+" "+this.b);
 }}
