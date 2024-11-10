60e31a831b080839c4ee19409ad770737d35714b
seethala e v <sheethalev@gmail.com>
2024-11-10 21:49:12 +0530

    call removed

--- SchoolProject/Home.java
+++ SchoolProject/Home.java
@@ -25,33 +25,23 @@ class Student
         total=maths+phy+chem;
     }
     
-    void AddStudentData()
-    {  
-        System.out.println("Enter name :");
-        name=s.nextLine();
-        System.out.println("Enter chemistry mark :");
-        chem=s.nextInt();
-        System.out.println("Enter maths mark :");
-        maths=s.nextInt();
-        System.out.println("Enter physics mark :");
-        phy=s.nextInt();
-    }
+    // void AddStudentData()
+    // {  
+    //     System.out.println("Enter name :");
+    //     name=s.nextLine();
+    //     System.out.println("Enter chemistry mark :");
+    //     chem=s.nextInt();
+    //     System.out.println("Enter maths mark :");
+    //     maths=s.nextInt();
+    //     System.out.println("Enter physics mark :");
+    //     phy=s.nextInt();
+    // }
 }
 public class Home 
 {
     public static void main(String args[])
     {
-       Student s1=new Student();
        
-       // hard coding class variables
-       s1.name="arun";
-       s1.maths=50;
-       s1.phy=20;
-       s1.chem=30;
-       s1.AddStudentData();
-       s1.getData();
-       s1.getTotal();
-       s1.getData();
       
     }
 }
