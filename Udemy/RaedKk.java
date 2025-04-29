b485237906ceb25e62ec4e5ce576db8b327864b6
seethala e v <sheethalev@gmail.com>
2025-04-29 22:52:46 +0530

    read int,float,string from keyboaerd

---
+++ Udemy/RaedKk.java
@@ -0,0 +1,15 @@
+package Udemy;
+
+import java.util.Scanner;
+
+public class RaedKk 
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+            int x=sc.nextInt();
+            System.out.println(x);
+            sc.close();
+    }
+}
