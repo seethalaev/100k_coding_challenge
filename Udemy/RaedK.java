308e949fdb5689ef7b36295bb5123b9eec47ceb0
seethala e v <sheethalev@gmail.com>
2025-04-29 21:01:10 +0530

    Read two intergers

--- Udemy/RaedK.java
+++ Udemy/RaedK.java
@@ -1,6 +1,17 @@
 package Udemy;
 
+import java.util.Scanner;
+
 public class RaedK 
 {
-    
+   public static void main(String args[]) 
+   {
+    Scanner s=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("enter two numbrs");
+    a=s.nextInt();
+    b=s.nextInt();
+    c=a+b;
+    System.out.println("SUM IS"+c);
+   }
 }
