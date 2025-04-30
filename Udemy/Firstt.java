3aac2f339899d5d306583ab69d8fd59498825abd
seethala e v <sheethalev@gmail.com>
2025-04-30 22:14:12 +0530

    name printing with nextline

---
+++ Udemy/Firstt.java
@@ -0,0 +1,16 @@
+package Udemy;
+
+import java.lang.*;
+import java.util.*;
+
+class Firstt
+	{
+	   public static void main(String args[])
+
+	{
+		String name;
+    		Scanner sc=new Scanner(System.in);
+         	System.out.println("May i know your name");
+		name=sc.nextLine();
+		System.out.println("Welcome"+name);
+}}
