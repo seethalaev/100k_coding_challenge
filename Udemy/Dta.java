e5263346249d511108028cce5bf70cb2f6ed206e
seethala e v <sheethalev@gmail.com>
2025-05-10 11:35:24 +0530

    range of datatype WITH MaxIMUM

--- Udemy/Dta.java
+++ Udemy/Dta.java
@@ -5,6 +5,7 @@ public class Dta
     public static void main(String args[])
     {
         System.out.println("Integre:" + Integer.MIN_VALUE);
-       
+        System.out.println("Integre:" + Integer.MAX_VALUE);
+        
     }
 }
