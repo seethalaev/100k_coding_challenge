9fba5c44ad529b94d69f8a7a04479689408efbdf
seethala e v <sheethalev@gmail.com>
2025-05-10 11:33:40 +0530

    range of datatype

---
+++ Udemy/Dta.java
@@ -0,0 +1,12 @@
+package Udemy;
+
+public class Dta 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Integre:" + Integer.MIN_VALUE);
+        System.out.println("Integre:" + Integer.MAX_VALUE);
+        System.out.println("Integre:" + Integer.BYTES);
+
+    }
+}
