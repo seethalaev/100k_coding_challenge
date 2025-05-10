cd35cc051d129dd3edcab2e745b43a64b4869f12
seethala e v <sheethalev@gmail.com>
2025-05-10 11:34:26 +0530

    range of datatype WITH MINIMUM

--- Udemy/Dta.java
+++ Udemy/Dta.java
@@ -5,8 +5,6 @@ public class Dta
     public static void main(String args[])
     {
         System.out.println("Integre:" + Integer.MIN_VALUE);
-        System.out.println("Integre:" + Integer.MAX_VALUE);
-        System.out.println("Integre:" + Integer.BYTES);
-
+       
     }
 }
