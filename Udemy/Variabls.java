96c2a389c27c8396d5a681ee871634a75e20e96a
seethala e v <sheethalev@gmail.com>
2025-05-10 20:42:58 +0530

    toBinaryString will convert integer into binary form

--- Udemy/Variabls.java
+++ Udemy/Variabls.java
@@ -7,16 +7,20 @@ class Variabls
 	   public static void main(String args[])
 
 	{
+		int a=8;
+		float A=7.6F;
 		byte b=120;
 		short s=300; 
-		int i=100; 
+		int i=-5; 
 		float f=23.3f; 
 		char c='s';
+		System.out.println(A);
+		System.out.println(a);
         System.out.println(b);
 		System.out.println(s);
 		System.out.println(i);
 		System.out.println(f);
 		System.out.println(c);
-
+		System.out.println(Integer.toBinaryString(i));
 
 }}
