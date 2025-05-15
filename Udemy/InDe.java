6147be5b4635aded5e489ce00c212c3d768cfc77
seethala e v <sheethalev@gmail.com>
2025-05-15 13:08:19 +0530

    2*4+3*2=8+6=14

--- Udemy/InDe.java
+++ Udemy/InDe.java
@@ -5,7 +5,11 @@ public class InDe
     public static void main(String args[])
     {
         int x=5,y;
-        y=x++;
+        y=x++; //y=++x -6,6
         System.out.println(x+" "+y);
+        int a=3, b=2,z;
+        z=2*++a +3 *b++;
+        System.out.println(z+" "+a);
+
     }
 }
