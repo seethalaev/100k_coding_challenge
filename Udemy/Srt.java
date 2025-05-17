d57e163634e3e318fdaa2de0511e682f7ef87fae
seethala e v <sheethalev@gmail.com>
2025-05-17 23:18:23 +0530

    String length,string uppercase

--- Udemy/Srt.java
+++ Udemy/Srt.java
@@ -7,8 +7,12 @@ public class Srt
         // String str2="java";
        // String str1=new String("java");
         String str2=new String("java");
-
         System.out.println(str1==str2);
+
+        String str3=new String("java");
+        System.out.println(str3.length());
+        String str4=str3.toUpperCase();
+        System.out.println(str4);
 
     }
 }
