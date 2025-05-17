09893cbc3a9bc38a81923a81fde05ddfd4f74012
seethala e v <sheethalev@gmail.com>
2025-05-17 21:45:25 +0530

    for same literal java will not create new object(true)

--- Udemy/Srt.java
+++ Udemy/Srt.java
@@ -4,7 +4,7 @@ public class Srt
     public static void main(String[] args)
      {
         String str1="java";
-        String str2="Java";
+        String str2="java";
         System.out.println(str1==str2);
 
     }
