48d63118a9e87de270d68faea3a5d867593b8c5e
seethala e v <sheethalev@gmail.com>
2025-05-17 21:44:34 +0530

    for same literal java will not create new object

---
+++ Udemy/Srt.java
@@ -0,0 +1,11 @@
+package Udemy;
+public class Srt
+ {
+    public static void main(String[] args)
+     {
+        String str1="java";
+        String str2="Java";
+        System.out.println(str1==str2);
+
+    }
+}
