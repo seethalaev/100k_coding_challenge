7622ad959f814ed985ccf87fa3e3cbe6a5691f17
seethala e v <sheethalev@gmail.com>
2025-05-17 21:50:08 +0530

    one obj created in pool one obj created in heap(false)

--- Udemy/Srt.java
+++ Udemy/Srt.java
@@ -3,8 +3,11 @@ public class Srt
  {
     public static void main(String[] args)
      {
-        String str1="java";
-        String str2="java";
+         String str1="java";
+        // String str2="java";
+       // String str1=new String("java");
+        String str2=new String("java");
+
         System.out.println(str1==str2);
 
     }
