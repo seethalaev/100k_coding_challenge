4dd4d282aa8874a6223a7f2f7fdd57fa3e6bc8d7
seethala e v <sheethalev@gmail.com>
2025-05-22 14:34:21 +0530

    startswith index nbr

--- Udemy/Srt.java
+++ Udemy/Srt.java
@@ -3,16 +3,20 @@ public class Srt
  {
     public static void main(String[] args)
      {
-         String str1="java";
-        // String str2="java";
-       // String str1=new String("java");
-        String str2=new String("java");
-        System.out.println(str1==str2);
+        //String str1="java";
+    // String str2="java";
+    // String str1=new String("java");
+        // String str2=new String("java");
+        // System.out.println(str1==str2);
 
-        String str3=new String("java");
-        System.out.println(str3.length());
-        String str4=str3.toUpperCase();
-        System.out.println(str4);
+        // String str3=new String("java");
+        // System.out.println(str3.length());
+        // String str4=str3.toUpperCase();
+        // System.out.println(str4);
+       
+        String str5=new String("Mr.shahrukh khan");
+        System.out.println(str5.startsWith("h",4));
+
 
     }
 }
