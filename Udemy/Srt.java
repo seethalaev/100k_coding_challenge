01adce730ad724c832c4b68bad1ed3090218d3d8
seethala e v <sheethalev@gmail.com>
2025-05-23 11:20:45 +0530

    equalignorecase

--- Udemy/Srt.java
+++ Udemy/Srt.java
@@ -3,8 +3,8 @@ public class Srt
  {
     public static void main(String[] args)
      {
-        //String str1="java";
-    // String str2="java";
+        String str1="java";
+     String str2="Java";
     // String str1=new String("java");
         // String str2=new String("java");
         // System.out.println(str1==str2);
@@ -20,8 +20,11 @@ public class Srt
         System.out.println(str5.indexOf("."));
 
         String str6=new String("www.selenium.com");
+        System.out.println(str6.indexOf("selenium"));
         System.out.println(str6.indexOf("?"));
         System.out.println(str6.indexOf(".",4));
         System.out.println(str6.lastIndexOf(".",50));
+
+        System.out.println(str1.equalsIgnoreCase(str6));
     }
 }
