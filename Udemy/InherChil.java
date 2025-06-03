6334a0751fe973f5402ed4a3cd5334381f647ec7
seethala e v <sheethalev@gmail.com>
2025-06-03 20:45:22 +0530

    colour printed

---
+++ Udemy/InherChil.java
@@ -0,0 +1,17 @@
+package Udemy;
+
+public class InherChil extends InherPara
+{
+    public void Engine()
+    {
+        System.out.println("engine works");
+    }
+    public void colour()
+    {
+        System.out.println(colour);
+    }
+    public static void main(String[] args) 
+    {
+        
+    }
+}
