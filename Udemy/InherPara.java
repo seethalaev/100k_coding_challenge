71a1febe6e50b6af6115323d0e1d253137885c4e
seethala e v <sheethalev@gmail.com>
2025-06-03 20:41:26 +0530

    parent class for inheritance

---
+++ Udemy/InherPara.java
@@ -0,0 +1,24 @@
+package Udemy;
+
+public class InherPara 
+{
+    String colour="red";
+    public void Break()
+    {
+        System.out.println("Break working");
+    }
+    public void Gear()
+    {
+        System.out.println("Gear works fine");
+    }
+    public void audeosystem()
+    {
+        System.out.println("audeo system works fine");
+
+    }
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
