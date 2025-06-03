bd6219ce10d7718549d96e1a0a6d017b5d8d4ca7
seethala e v <sheethalev@gmail.com>
2025-06-03 21:36:37 +0530

    array pgm

---
+++ Udemy/Arra.java
@@ -0,0 +1,19 @@
+package Udemy;
+
+public class Arra 
+{
+    public static void main(String[] args) {
+        
+    
+    int a[]=new int[5];
+    a[0]=1;
+     a[1]=9;
+      a[2]=6;
+       a[3]=5;
+        a[4]=3;
+        for(int i=0;i<a.length-1;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+}}
