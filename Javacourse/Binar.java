07484d21cad31badebf49667899cebdffa7352b4
seethala e v <sheethalev@gmail.com>
2025-06-15 22:32:42 +0530

    binary convertion

---
+++ Javacourse/Binar.java
@@ -0,0 +1,15 @@
+package Javacourse;
+
+import java.awt.*;
+import javax.swing.*;
+import java.lang.*;
+
+
+public class Binar {
+    public static void main(String[] args) {
+        int x=-5;//5
+        
+        System.out.println(Integer.toBinaryString(x));
+    }
+    
+}
