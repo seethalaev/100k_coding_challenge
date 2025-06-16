7d2ddd388a3cf6b60fd14bcf6057630b7476a225
seethala e v <sheethalev@gmail.com>
2025-06-16 22:20:32 +0530

    remov spl characters

---
+++ Javacourse/Spcl.java
@@ -0,0 +1,20 @@
+package Javacourse;
+
+public class Spcl 
+{
+    public static void main(String[] args)
+    {
+        //REMOV SPCL CHARCTRS
+        String str1="AcB1@SD%@23";
+        System.out.println(str1.replaceAll("[^a-zA-Z0-9]", ""));
+        
+        //remove spaces
+        String str2="    abc     sksa    jjja";
+       //  System.out.println(str2.replaceAll("[\\s]+", " "));
+        System.out.println(str2.replaceAll("[\\s]+","  ").trim());
+
+        //length of words
+        String words[]=str2.split("[//s]");
+        System.out.println(words.length);
+    }
+}
