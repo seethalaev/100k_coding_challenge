d83023fd9dadd1dcbb9a5e2b7790eaae684adccf
seethala e v <sheethalev@gmail.com>
2025-06-17 20:36:24 +0530

    octal,decimal,hexadecimal  radix

--- Javacourse/Radix.java
+++ Javacourse/Radix.java
@@ -5,16 +5,34 @@ import java.util.Scanner;
 public class Radix {
     public static void main(String[] args)
      {
-        
+        //else if ladder 
         Scanner sc=new Scanner(System.in);
         String num;
         System.out.println("enter number");
         num=sc.nextLine();
 
-        if(num.matches("[0-1]+"))
+        if(num.matches("[01]+"))
         {
-            System.out.println("binary radix");
+            System.out.println("binary radix=2");
+        }
+        else if(num.matches("[0-7]+"))
+            {
+                 System.out.println("octal radix=8");
+            }
+           else if(num.matches("[0-9]+"))
+            {
+                 System.out.println("Decimal radix=10");
+            }
+            else if(num.matches("[0-9AF]+"))
+            {
+                 System.out.println("hexa decimal=16");
+            }
+           else
+           {
+            System.out.println("not a number");
+           }
+           
         }
     }
     
-}
+
