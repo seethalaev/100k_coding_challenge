70d7090a78787ecc36c27758c6ac6917cb7eafda
seethala e v <sheethalev@gmail.com>
2025-06-30 18:44:18 +0530

    count digits

--- Javacourse/F.java
+++ Javacourse/F.java
@@ -6,14 +6,18 @@ public class F
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter limit");
-        int n=5;
-        int fact=1;
-        for(int i=1;i<n;i++)
-        {
-            fact=fact*i;
-            System.out.println(fact);
-        }
-}}
+       // Scanner sc=new Scanner(System.in);
+       // System.out.println("enter limit");
+      int n=2345;
+      int i=0;
+      int count=0;
+      
+      while(n>0)
+      {
+        n=n/10;
+        count++;
+      }
+      System.out.println(count);
+          }    }
+
 
