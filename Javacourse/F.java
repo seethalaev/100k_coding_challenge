a22f403d07e2b908f91d3a0ad22a5ffc85f4cf33
seethala e v <sheethalev@gmail.com>
2025-06-30 21:49:58 +0530

    palindrome

--- Javacourse/F.java
+++ Javacourse/F.java
@@ -6,23 +6,31 @@ public class F
 {
     public static void main(String args[])
     {
-       
-        int n=325;
-        int r=0;
-        int rev=0;
-        while(n>0)
-        {
-            r=n%10;                      //5   2     3
-            System.out.println(r);       //
-            n=n/10;                      //32   3    0
-            System.out.println(n);       //
-            rev=r+rev*10;                //5    52   3+520
-            System.out.println(rev);     //
+       int n=32523;
+       int r=0;
+       int rev=0;
+       int m=n;
+       while(n>0)
+    
+       {
+        r=n%10;         //
+        n=n/10;
+        System.out.println(r);
+        rev=r+rev*10;
+       }
 
-           
+        if(rev==m)
+        {
+            System.out.println("palindrome");
         }
-    }
-    }
+        else
+        {
+            System.out.println("noooo");
+        }
+       }   
+        }
+    
+    
         
 
 
