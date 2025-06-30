e8a245af53c7708987fb0f6b364f37a395cdc1a3
seethala e v <sheethalev@gmail.com>
2025-06-30 22:11:14 +0530

    print digits

--- Javacourse/F.java
+++ Javacourse/F.java
@@ -6,29 +6,37 @@ public class F
 {
     public static void main(String args[])
     {
-       int n=32523;
-       int r=0;
-       int rev=0;
-       int m=n;
-       while(n>0)
-    
-       {
-        r=n%10;         //
-        n=n/10;
-        System.out.println(r);
-        rev=r+rev*10;
-       }
 
-        if(rev==m)
+        int r=0;
+        String str="";
+        int n=159;
+        while(n>0)
         {
-            System.out.println("palindrome");
-        }
-        else
-        {
-            System.out.println("noooo");
-        }
-       }   
-        }
+            r=n%10;
+            n=n/10;
+            str=str+r;
+            
+        }System.out.println(str);
+        char c;
+         for(int i=str.length()-1;i>=0;i--)
+         {
+            c=str.charAt(i);
+            switch (c) {
+                case '9':
+                    System.out.println("nine");
+                    break;
+                    case '1':
+                    System.out.println("one");
+                    break;
+                    case '5':
+                    System.out.println("five");
+                    break;
+            
+                default:
+                    break;
+            }
+         }
+        }}
     
     
         
