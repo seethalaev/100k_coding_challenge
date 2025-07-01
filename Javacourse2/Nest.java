491d9e0201fef68e3bb3ea51579d1e17f5b0215b
seethala e v <sheethalev@gmail.com>
2025-07-01 16:47:16 +0530

    nested loop

---
+++ Javacourse2/Nest.java
@@ -0,0 +1,19 @@
+package Javacourse2;
+
+public class Nest 
+{
+   
+    public static void main(String[] args) {
+         int i;int j;
+        for( i=1;i<=5;i++)
+        {
+            for( j=1;j<=5;j++)
+            {
+                 System.out.print("("+i+","+j+") ");
+            }
+            System.out.println("");
+           
+        }
+       
+    }
+}
