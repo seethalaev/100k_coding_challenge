d7d46ffcec48a89f989f01fb4951cd543ced2e99
seethala e v <sheethalev@gmail.com>
2025-07-01 22:25:56 +0530

    123-123-1213

---
+++ Javacourse2/P1.java
@@ -0,0 +1,17 @@
+package Javacourse2;
+
+public class P1 
+{
+    public static void main(String[] args) {
+        {
+            for(int i=1;i<=7;i++)
+            {
+                for( int j=1;j<=7;j++)
+                {
+                    System.out.print(j+" ");
+                }
+                System.out.println(" ");
+            }
+        }
+    }
+}
