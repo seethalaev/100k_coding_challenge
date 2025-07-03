c2871dc06c28d2f82ab62066103d8806df216cc8
seethala e v <sheethalev@gmail.com>
2025-07-03 22:24:59 +0530

    pattern4

---
+++ Javacourse2/P4.java
@@ -0,0 +1,20 @@
+package Javacourse2;
+
+public class P4 {
+    public static void main(String[] args) {
+        int count=0;
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                //.out.println("shilpa "+i);
+                //System.out.println("seethu "+j);
+                count++;
+                //System.out.format("02d%",count);
+                System.out.print("count "+count);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
