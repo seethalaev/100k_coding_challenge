46106cc0c7e4515399a46a3832b1798115f17d6f
seethala e v <sheethalev@gmail.com>
2025-07-03 22:25:34 +0530

    pattern5

---
+++ Javacourse2/P5.java
@@ -0,0 +1,16 @@
+package Javacourse2;
+
+
+public class P5 {
+    public static void main(String[] args) {
+       
+       for(int i=0;i<=5;i++)
+       {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(j+" ");
+        }
+        System.out.println("");
+       }}}
+        
+    
